66c788c8d1b453a934e0f901ba360c840b1adbc0
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-05-20 12:12:51 -0400

    debugging

--- plugins/jobs/custom_job_data.rb
+++ plugins/jobs/custom_job_data.rb
@@ -2,8 +2,8 @@ module AresMUSH
   module Jobs
     def self.custom_job_menu_fields(job, viewer)
       sheet = viewer.sheet.template_config
-      sheet[:id] = viewer.id
-      sheet[:name] = viewer.name
+      sheet['id'] = viewer.id
+      sheet['name'] = viewer.name
       [:template, :health, :agg_wounds, :lethal_wounds, :bashing_wounds].each do |f|
         sheet[f] = viewer.sheet.public_send(f)
       end
